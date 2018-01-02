f5b1295f37751b457342e581bbd2502425dd7c15
Chad Montoya <chadmontoya21@yahoo.com>
2018-01-02 04:10:51 +0000

    helper method for #make_apps

--- lib/daily_trending/app.rb
+++ lib/daily_trending/app.rb
@@ -3,15 +3,21 @@ class DailyTrending::App
   @@all = []
 
   def self.today
-    self.scrape_play_store
+    self.make_apps
   end
-
 
 
   def self.scrape_play_store
     doc = Nokogiri::HTML(open("https://play.google.com/store/apps/collection/promotion_3000792_new_releases_apps?clp=SpEBCikKI3Byb21vdGlvbl8zMDAwNzkyX25ld19yZWxlYXNlc19hcHBzEAcYAxpkCl5uZXdfaG9tZV9kZXZpY2VfZmVhdHVyZWRfcmVjczJfdG9waWNfdjFfbGF1bmNoX2FwcHNfVVNfXzE1MTQ0NDgwMDAwMDBfNl9wcm9tb18xNTE0NDkxNzcwMTgwMDAwEAwYAw%3D%3D%3AS%3AANO1ljLrBj0&hl=en"))
+    doc.css('div.card-content.id-track-click.id-track-impression')
+  end
 
-    doc.css('div.card-content.id-track-click.id-track-impression').each do |a|
+
+    # Iterates  #scrape_play_store method to # Makes the apps and assigns
+    #  the attributes available on Google play store
+
+  def self.make_apps
+    scrape_play_store.each do |a|
       app = self.new
       app.title = a.css('a.title').attribute('title').value
       app.dev = a.css('a.subtitle').attribute('title').value
@@ -23,6 +29,23 @@ class DailyTrending::App
     end
     @@all
   end
+
+
+  # def self.scrape_play_store
+  #   doc = Nokogiri::HTML(open("https://play.google.com/store/apps/collection/promotion_3000792_new_releases_apps?clp=SpEBCikKI3Byb21vdGlvbl8zMDAwNzkyX25ld19yZWxlYXNlc19hcHBzEAcYAxpkCl5uZXdfaG9tZV9kZXZpY2VfZmVhdHVyZWRfcmVjczJfdG9waWNfdjFfbGF1bmNoX2FwcHNfVVNfXzE1MTQ0NDgwMDAwMDBfNl9wcm9tb18xNTE0NDkxNzcwMTgwMDAwEAwYAw%3D%3D%3AS%3AANO1ljLrBj0&hl=en"))
+  #
+  #   doc.css('div.card-content.id-track-click.id-track-impression').each do |a|
+  #     app = self.new
+  #     app.title = a.css('a.title').attribute('title').value
+  #     app.dev = a.css('a.subtitle').attribute('title').value
+  #     app.dev_url = ("https://play.google.com" + a.css('a.subtitle').attribute('href').value)
+  #     app.app_url = ("https://play.google.com" + a.css('a.title').attribute('href').value)
+  #     app.rating = a.css('div.tiny-star').attribute('aria-label').value.strip.slice(/\d.\S/)<<"/5 Stars"
+  #     app.price = a.at_css('span.display-price').text
+  #     @@all << app
+  #   end
+  #   @@all
+  # end
 
 
 
