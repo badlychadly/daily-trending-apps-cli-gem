e11e0e0cdfb806cb6cc7e5b557b1eec0e2c8468d
Chad Montoya <chadmontoya21@yahoo.com>
2018-01-03 22:39:30 +0000

    bug fix

--- lib/daily_trending.rb
+++ lib/daily_trending.rb
@@ -4,6 +4,5 @@ require 'pry'
 require 'colorize'
 require 'colorized_string'
 
-require_relative './daily_trending/version'
 require_relative  './daily_trending/app'
 require_relative  './daily_trending/cli'
